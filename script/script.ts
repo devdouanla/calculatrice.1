24d8104a4609603294c030674275d0f190d79e20
devdouanla <devdouanla>
2024-12-11 00:48:45 +0100

    modification

--- script/script.ts
+++ script/script.ts
@@ -1,95 +1,140 @@
+
 // HTML Elements
 const btn: any = document.querySelectorAll('.button');
+const lsci: any = document.querySelectorAll('.lsci');
 
-let result = document.querySelector('.reponse') as HTMLElement 
-let saisie = document.querySelector('.saisie') as HTMLElement 
- let egale: any = document.querySelector('.buttadd');
- let supp: any = document.querySelector('.buttsupp');
-let sin  = document.querySelector('.sin') as HTMLElement 
-let cos = document.querySelector('.cos') as HTMLElement 
-let tan = document.querySelector('.tan') as HTMLElement 
-let sqrt = document.querySelector('.sqrt') as HTMLElement 
-let exp = document.querySelector('.exp') as HTMLElement 
-let log = document.querySelector('.log') as HTMLElement 
-let puiss = document.querySelector('.puiss') as HTMLElement 
+let result = document.querySelector('.reponse') as HTMLElement
+let saisie = document.querySelector('.saisie') as HTMLElement
+let egale: any = document.querySelector('.buttadd');
+let supp: any = document.querySelector('.buttsupp');
+let sin = document.querySelector('.sin') as HTMLElement
+let cos = document.querySelector('.cos') as HTMLElement
+let tan = document.querySelector('.tan') as HTMLElement
+let sqrt = document.querySelector('.sqrt') as HTMLElement
+let exp = document.querySelector('.exp') as HTMLElement
+let log = document.querySelector('.log') as HTMLElement
+let pu = document.querySelector('.puiss') as HTMLElement
+let dec = document.querySelector('.dec') as HTMLElement
+let hex = document.querySelector('.hex') as HTMLElement
+let bin = document.querySelector('.bin') as HTMLElement
+let facto = document.querySelector('.facto') as HTMLElement
+let heu = document.querySelector('.heu') as HTMLElement
+let mode = document.querySelector('.mode') as HTMLElement
 
 
 
 
 
-function affiche( value: string):string{
-     
-   let text1: string= saisie.innerText   
-saisie.innerText = text1+value;
-return saisie.innerText
+
+function affiche(value: string): string {
+
+     let text1: string = saisie.innerText
+     saisie.innerText = text1 + value;
+     return saisie.innerText
 }
-function del (h:HTMLElement){
+function del(h: HTMLElement) {
      h.innerText = "";
 }
+function degreesToRadians(degrees: number): number {
+     return degrees * (Math.PI / 180);
+ }
+ 
+ 
+
+
+
 
 sin.addEventListener('click', () => {
-     const ce : any = saisie.innerText
+     const ce: any|number = saisie.innerText
      let s: any = " "
-     if ( typeof ce === 'number') 
-          console.log(ce)
-             result.innerText = Math.sin(ce).toString();
-  
+     result.innerText = Math.sin(degreesToRadians(ce)).toString();
+   
+
 
 });
 cos.addEventListener('click', () => {
-     const ce : any = saisie.innerText
+     const ce: any = saisie.innerText
      let s: any = " "
-     if ( typeof ce === 'number') 
-          console.log(ce)
-             result.innerText = Math.cos(ce).toString();
-  
+
+
+
+     
+          result.innerText = Math.cos(degreesToRadians(ce)).toString();
+   
 
 });
 tan.addEventListener('click', () => {
-     const ce : any = saisie.innerText
+     const ce: any = saisie.innerText
      let s: any = " "
-     if ( typeof ce === 'number') 
-          console.log(ce)
-             result.innerText = Math.tan(ce).toString();
-  
+   
+          result.innerText = Math.tan(degreesToRadians(ce)).toString();
+     
+
+
+
+
 
 });
 sqrt.addEventListener('click', () => {
-     const ce : any = saisie.innerText
+     const ce: any = saisie.innerText
      let s: any = " "
-     if ( typeof ce === 'number') 
-          console.log(ce)
-             result.innerText = Math.sqrt(ce).toString();
-  
+    
+          result.innerText = Math.sqrt(ce).toString();
+     
+
+
+
+
+
 
 });
 exp.addEventListener('click', () => {
-     const ce : any = saisie.innerText
+     const ce: any = saisie.innerText
      let s: any = " "
-     if ( typeof ce === 'number') 
-          console.log(ce)
-             result.innerText = Math.exp(ce).toString();
-  
+
+
+
+   
+          result.innerText = Math.exp(ce).toString();
+    
 
 });
 log.addEventListener('click', () => {
-     const ce : any = saisie.innerText
+     const ce: any = saisie.innerText
      let s: any = " "
-     if ( typeof ce === 'number') 
-          console.log(ce)
-             result.innerText = Math.log10(ce).toString();
-  
+
+
+
+   
+          result.innerText = Math.log10(ce).toString();
+    
+
+
 
 });
 
 
-puiss.addEventListener('click', () => {
-     const ce : any = saisie.innerText
-     let s: any = " "
-     if ( typeof ce === 'number') 
-          console.log(ce)
-             result.innerText = Math.pow(ce).toString();
-  
+pu.addEventListener('click', () => {
+     saisie.innerText = saisie.innerText + "**"
+
+
+});
+mode.addEventListener('click', () => {
+     if (mode.innerText == "normal") {
+          lsci.forEach(element => {
+               element.classList.remove("enleve")
+               mode.innerText = "scientific"
+          });
+
+     } else if (mode.innerText == "scientific") {
+
+          lsci.forEach(element => {
+               element.classList.add("enleve")
+               mode.innerText = "normal"
+          });
+
+     }
+
 
 });
 
@@ -113,27 +158,92 @@ puiss.addEventListener('click', () => {
 
 
 
-  egale.addEventListener('click', () => {
-     try{
-   const ce = eval(saisie.innerText)
+egale.addEventListener('click', () => {
+     try {
+          const ce = eval(saisie.innerText)
 
-   if (ce==Infinity){
-     result.innerText ='MATH ERROR';
-   }else{
-     result.innerText = ce;
-   }
-  }catch(err){
-   result.innerText= 'SYNTHAX ERROR'
-   }
+          if (ce == Infinity) {
+               result.innerText = 'MATH ERROR';
+          } else {
+               result.innerText = ce;
+          }
+     } catch (err) {
+          result.innerText = 'SYNTHAX ERROR'
+     }
 });
 for (let i = 0; i < btn.length; i++) {
      btn[i].addEventListener('click', () => {
           affiche(btn[i].innerText)
      });
- }
+}
 
 
 supp.addEventListener('click', () => {
-     del (result );
+     del(result);
      del(saisie)
 })
+
+function fact(n: number): number {
+     if (n < 0) {
+          throw new Error("Le factoriel n'est pas défini pour les nombres négatifs");
+     }
+     let result = 1;
+     for (let i = 2; i <= n; i++) {
+          result *= i;
+     }
+     return result;
+}
+function convertToBinary(num: number): string {
+     if (!Number.isInteger(num)) {
+          return "Erreur : Seuls les nombres entiers peuvent être convertis en binaire.";
+     }
+     return num.toString(2);
+}
+
+bin.addEventListener('click', () => {
+     const ce: any = saisie.innerText
+   
+result.innerText = convertToBinary(Number(ce)).toString();
+     
+
+})
+
+hex.addEventListener('click', () => {
+     const ce: any = saisie.innerText
+let v = Number(ce)
+
+let hex =v.toString(16)
+    
+          result.innerText = hex
+   
+})
+dec.addEventListener('click', () => {
+     const ce: any = saisie.innerText
+     const bi = /^[01]+$/.test(ce)
+     if (bi) {
+          result.innerText = parseInt(ce, 2).toString()
+     } else {
+          result.innerText = parseInt(ce, 16).toString()
+
+     }
+
+
+
+
+
+
+
+})
+
+function afficherHeure(): string {
+     const now = new Date(); // Crée un objet Date représentant l'heure actuelle
+
+     const hour = now.getHours().toString().padStart(2, '0'); // Récupère l'heure, avec un format à 2 chiffres
+     const minutes = now.getMinutes().toString().padStart(2, '0'); // Récupère les minutes, format à 2 chiffres
+     const secondes = now.getSeconds().toString().padStart(2, '0'); // Récupère les secondes, format à 2 chiffres
+
+     // Retourner l'heure au format "HH:MM:SS"
+     return hour + ':' + minutes + ':' + secondes;
+}
+heu.innerText = afficherHeure();
+
--- ts-built/script.js
+++ ts-built/script.js
@@ -1,5 +1,6 @@
 // HTML Elements
 var btn = document.querySelectorAll('.button');
+var lsci = document.querySelectorAll('.lsci');
 var result = document.querySelector('.reponse');
 var saisie = document.querySelector('.saisie');
 var egale = document.querySelector('.buttadd');
@@ -10,7 +11,13 @@ var tan = document.querySelector('.tan');
 var sqrt = document.querySelector('.sqrt');
 var exp = document.querySelector('.exp');
 var log = document.querySelector('.log');
-var puiss = document.querySelector('.puiss');
+var pu = document.querySelector('.puiss');
+var dec = document.querySelector('.dec');
+var hex = document.querySelector('.hex');
+var bin = document.querySelector('.bin');
+var facto = document.querySelector('.facto');
+var heu = document.querySelector('.heu');
+var mode = document.querySelector('.mode');
 function affiche(value) {
     var text1 = saisie.innerText;
     saisie.innerText = text1 + value;
@@ -19,54 +26,55 @@ function affiche(value) {
 function del(h) {
     h.innerText = "";
 }
+function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
 sin.addEventListener('click', function () {
     var ce = saisie.innerText;
     var s = " ";
-    if (typeof ce === 'number')
-        console.log(ce);
-    result.innerText = Math.sin(ce).toString();
+    result.innerText = Math.sin(degreesToRadians(ce)).toString();
 });
 cos.addEventListener('click', function () {
     var ce = saisie.innerText;
     var s = " ";
-    if (typeof ce === 'number')
-        console.log(ce);
-    result.innerText = Math.cos(ce).toString();
+    result.innerText = Math.cos(degreesToRadians(ce)).toString();
 });
 tan.addEventListener('click', function () {
     var ce = saisie.innerText;
     var s = " ";
-    if (typeof ce === 'number')
-        console.log(ce);
-    result.innerText = Math.tan(ce).toString();
+    result.innerText = Math.tan(degreesToRadians(ce)).toString();
 });
 sqrt.addEventListener('click', function () {
     var ce = saisie.innerText;
     var s = " ";
-    if (typeof ce === 'number')
-        console.log(ce);
     result.innerText = Math.sqrt(ce).toString();
 });
 exp.addEventListener('click', function () {
     var ce = saisie.innerText;
     var s = " ";
-    if (typeof ce === 'number')
-        console.log(ce);
     result.innerText = Math.exp(ce).toString();
 });
 log.addEventListener('click', function () {
     var ce = saisie.innerText;
     var s = " ";
-    if (typeof ce === 'number')
-        console.log(ce);
     result.innerText = Math.log10(ce).toString();
 });
-puiss.addEventListener('click', function () {
-    var ce = saisie.innerText;
-    var s = " ";
-    if (typeof ce === 'number')
-        console.log(ce);
-    result.innerText = Math.pow(ce).toString();
+pu.addEventListener('click', function () {
+    saisie.innerText = saisie.innerText + "**";
+});
+mode.addEventListener('click', function () {
+    if (mode.innerText == "normal") {
+        lsci.forEach(function (element) {
+            element.classList.remove("enleve");
+            mode.innerText = "scientific";
+        });
+    }
+    else if (mode.innerText == "scientific") {
+        lsci.forEach(function (element) {
+            element.classList.add("enleve");
+            mode.innerText = "normal";
+        });
+    }
 });
 egale.addEventListener('click', function () {
     try {
@@ -94,3 +102,48 @@ supp.addEventListener('click', function () {
     del(result);
     del(saisie);
 });
+function fact(n) {
+    if (n < 0) {
+        throw new Error("Le factoriel n'est pas défini pour les nombres négatifs");
+    }
+    var result = 1;
+    for (var i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+function convertToBinary(num) {
+    if (!Number.isInteger(num)) {
+        return "Erreur : Seuls les nombres entiers peuvent être convertis en binaire.";
+    }
+    return num.toString(2);
+}
+bin.addEventListener('click', function () {
+    var ce = saisie.innerText;
+    result.innerText = convertToBinary(Number(ce)).toString();
+});
+hex.addEventListener('click', function () {
+    var ce = saisie.innerText;
+    var v = Number(ce);
+    var hex = v.toString(16);
+    result.innerText = hex;
+});
+dec.addEventListener('click', function () {
+    var ce = saisie.innerText;
+    var bi = /^[01]+$/.test(ce);
+    if (bi) {
+        result.innerText = parseInt(ce, 2).toString();
+    }
+    else {
+        result.innerText = parseInt(ce, 16).toString();
+    }
+});
+function afficherHeure() {
+    var now = new Date(); // Crée un objet Date représentant l'heure actuelle
+    var hour = now.getHours().toString().padStart(2, '0'); // Récupère l'heure, avec un format à 2 chiffres
+    var minutes = now.getMinutes().toString().padStart(2, '0'); // Récupère les minutes, format à 2 chiffres
+    var secondes = now.getSeconds().toString().padStart(2, '0'); // Récupère les secondes, format à 2 chiffres
+    // Retourner l'heure au format "HH:MM:SS"
+    return hour + ':' + minutes + ':' + secondes;
+}
+heu.innerText = afficherHeure();
